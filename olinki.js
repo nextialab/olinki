2bc4c97435aa4d07bf8696bd1fd013d385a8738f
Nelson I. Gonzalez <nigm2005@gmail.com>
2017-09-07 17:43:33 -0500

    Integrating git

--- olinki.js
+++ olinki.js
@@ -44,6 +44,7 @@ const app = express();
 
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('body-parser').json());
 app.use(require('express-session')({
     secret: process.env.SECRET_KEY,
     resave: false,
